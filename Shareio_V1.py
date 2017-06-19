6549b723703be39a8c28ce2df9e972d442c3227e
Daniel O'Brien <dob_91@outlook.ie>
2017-06-19 11:10:09 +0100

    Add files via upload

---
+++ Shareio_V1.py
@@ -0,0 +1,17 @@
+#BETA
+print("Welcome to Share.io")
+first=(int(input("Please enter the Amount of Money you are sharing/dividing")))
+print("In this program we will use the following variables in our Ratio; x:y")
+second=(int(input("Please enter the number for 'x'")))
+third=(int(input("Please enter the number for 'y'")))
+fourth = second+third
+fifth = first/fourth
+sixth = second*fifth
+seventh = third*fifth
+sixth = (str(sixth))
+seventh = (str(seventh))
+print("Your Ratio is.........")
+print(sixth + ":" + seventh)
+print("Thank You for using Share.io!!")
+#Copyright Daniel O'Brien/Share.io 2017
+#Developed by @daniel-obrien
